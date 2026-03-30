f02e52585aceae33119d2cc118690f6e82457ef5
Alexander Kostromin <alexanderkostromin@Alexanders-Mac-mini.local>
2026-03-30 13:07:00 +0500

    feat(models, tests): add handmadelstm model and tests for it

--- scripts/models.py
+++ scripts/models.py
@@ -39,3 +39,56 @@ class HandmadeRNN(nn.Module):
         logits = full_hidden_states @ self.Why + self.by
 
         return logits, hidden
+
+
+class HandmadeLSTM(nn.Module):
+    def __init__(self, vocab_size, hidden_size, emb_dim):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.emb = nn.Embedding(vocab_size, emb_dim)
+
+        self.Wxh = nn.Parameter(torch.empty(emb_dim, 4 * hidden_size))
+        self.Whh = nn.Parameter(torch.empty(hidden_size, 4 * hidden_size))
+        self.Why = nn.Parameter(torch.empty(hidden_size, vocab_size))
+        nn.init.xavier_uniform_(self.Wxh)
+        nn.init.xavier_uniform_(self.Whh)
+        nn.init.xavier_uniform_(self.Why)
+
+        self.bh = nn.Parameter(torch.zeros(4 * hidden_size))
+        self.by = nn.Parameter(torch.zeros(vocab_size))
+
+    def forward(self, x, init_states=None):
+        emb = self.emb(x)   # [B, S, E]
+        batch_size, seq_len, _ = emb.size()
+
+        if init_states is None:
+            h_t = torch.zeros(batch_size, self.hidden_size).to(x.device)
+            c_t = torch.zeros(batch_size, self.hidden_size).to(x.device)
+        else:
+            h_t, c_t = init_states
+
+        outputs = []
+
+        for t in range(seq_len):
+            x_t = emb[:, t, :]
+
+            gates = x_t @ self.Wxh + h_t @ self.Whh + self.bh
+
+            i_gate, f_gate, g_gate, o_gate = gates.chunk(4, dim=1)
+
+            i = torch.sigmoid(i_gate)    # Input gate
+            f = torch.sigmoid(f_gate)    # Forget gate
+            o = torch.sigmoid(o_gate)    # Output gate
+            g = torch.tanh(g_gate)       # cell state
+
+            c_t = f * c_t + i * g
+
+            h_t = o * torch.tanh(c_t)
+
+            outputs.append(h_t.unsqueeze(1))
+
+        full_output = torch.cat(outputs, dim=1)
+        logits = full_output @ self.Why + self.by
+
+        return logits, (h_t, c_t)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,28 @@
 import torch
 import pytest
-from scripts.models import HandmadeRNN
+from scripts.models import HandmadeRNN, HandmadeLSTM
 
 
-@pytest.mark.parametrize('vocab_size, hidden_size, emb_dim, batch_size, seq_len, result', [
-    (100, 32, 16, 4, 10, [4, 10, 100])
+@pytest.mark.parametrize('vocab_size, hidden_size, emb_dim, batch_size, seq_len', [
+    (100, 32, 16, 4, 10)
 ])
-def test_handmadernn(vocab_size, hidden_size, emb_dim, batch_size, seq_len, result):
+def test_handmadernn(vocab_size, hidden_size, emb_dim, batch_size, seq_len):
     model = HandmadeRNN(vocab_size, hidden_size, emb_dim)
     test_input = torch.randint(0, 100, (batch_size, seq_len))
     logits, last_h = model(test_input)
     assert logits.shape == (batch_size, seq_len, vocab_size)
     assert last_h.shape == (batch_size, hidden_size)
+    assert not torch.isnan(logits).any()
+
+
+@pytest.mark.parametrize('vocab_size, hidden_size, emb_dim, batch_size, seq_len', [
+    (100, 32, 16, 4, 10)
+])
+def test_handmadelstm(vocab_size, hidden_size, emb_dim, batch_size, seq_len):
+    model = HandmadeLSTM(vocab_size, hidden_size, emb_dim)
+    test_input = torch.randint(0, 100, (batch_size, seq_len))
+    logits, (last_h, last_c) = model(test_input)
+    assert logits.shape == (batch_size, seq_len, vocab_size)
+    assert last_h.shape == (batch_size, hidden_size)
+    assert last_c.shape == (batch_size, hidden_size)
     assert not torch.isnan(logits).any()
